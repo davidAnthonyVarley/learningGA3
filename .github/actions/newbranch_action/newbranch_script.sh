bed097790f42f757413c30fd7eff4c7426f2f06c
Varley <dvarley@deloitte.ie>
2025-06-10 14:42:29 +0100

    testing GA 14.39

--- .github/actions/newbranch_action/newbranch_script.sh
+++ .github/actions/newbranch_action/newbranch_script.sh
@@ -1,15 +1,2 @@
 #!/bin/bash
 
-echo "****************"
-echo "hello from my composite script. you have just created a new branch"
-        echo "this is your input:"
-        echo "\t${{inputs.brancher_name}}"
-        echo "\t${{inputs.branch_name}}"
-        echo ""
-        name="${{inputs.brancher_name}}"
-        branch="${{inputs.branch_name}}"
-        message="${name}, you have created the ${branch}!"
-        echo "\t message to be accessed by output in workflow${message}"
-
-        echo "output-from-script=${message}" >> $GITHUB_OUTPUT
-        echo "****************"
